abfabb34c24c75fa315ebc6d2e6d3908f7318a0c
Ömer Faruk KAYALAR <omer.kayalar0101@icloud.com>
2022-07-10 14:20:04 +0300

    feat: mail service

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 const app = require('./app');
 const appLogger = require('./utils/appLogger');
+const MailService = require('./configs/mailer.config');
 
 const PORT = 3005 || process.env.PORT;
 
 app.listen(PORT, (err) => {
   if (!err) {
-    return appLogger.log(`Server is live. PORT : ${PORT}`);
+    appLogger.log(`Server is live. PORT : ${PORT}`);
+    return MailService.verify((error) => {
+      if (error) {
+        appLogger.error('Mail server cannot be reached!');
+      } else {
+        appLogger.log('Mail server is ready!');
+      }
+    });
   }
 
   return appLogger.error(`Something went wrong!`);
