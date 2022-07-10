42ce45ac32a97e25324120449099fb0aae97b1c7
Ömer Faruk KAYALAR <omer.kayalar0101@icloud.com>
2022-07-10 14:19:35 +0300

    beautify

--- src/app.js
+++ src/app.js
@@ -5,9 +5,26 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '50mb', extended: true }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors({ origin: ['http://localhost:3005', 'https://3ddonate.com'] }));
+app.use(
+  bodyParser.json({
+    limit: '50mb',
+    extended: true,
+  })
+);
+app.use(
+  express.urlencoded({
+    limit: '50mb',
+    extended: true,
+  })
+);
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3005',
+      'https://3ddonate.com',
+    ],
+  })
+);
 
 app.use('/api/', routes);
 
